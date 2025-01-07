ee8a0b698e21e3e6a962c0a15ea407133d185887
Sam Moore <sam.moore@penten.com>
2025-01-07 15:18:06 +1100

    replace telescope with fzf-lua

--- init.lua
+++ init.lua
@@ -238,11 +238,6 @@ vim.cmd [[xnoremap <expr> A mode() ==# "V" ? "<C-v>$A" : "A"]]
 vim.cmd [[xnoremap <expr> A mode() ==# "V" ? "<C-v>$A" : "A"]]
 
 -- C-p find files
-vim.keymap.set('n', '<C-p>', ':Telescope find_files<CR>')
-vim.keymap.set('i', '<C-p>', '<esc>:Telescope find_files<CR>')
-vim.keymap.set('n', '<C-g>', ':Telescope live_grep<CR>')
-vim.keymap.set('i', '<C-g>', '<esc>:Telescope live_grep<CR>')
-vim.keymap.set('n', '<leader>o', ':Telescope lsp_document_symbols<CR>')
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 
 -- Diagnostic keymaps
@@ -448,6 +443,8 @@ require('lazy').setup({
 
       -- Allows extra capabilities provided by blink.cmp
       'saghen/blink.cmp',
+
+      'ibhagwan/fzf-lua',
     },
     config = function()
       -- Brief aside: **What is LSP?**
@@ -495,27 +492,32 @@ require('lazy').setup({
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          local fzf_lua = require 'fzf-lua'
+          map('gd', function()
+            fzf_lua.lsp_definitions { jump_to_single_result = true, cwd_only = false }
+          end, '[G]oto [D]efinition')
 
           -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('gr', fzf_lua.lsp_references, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('gI', fzf_lua.lsp_implementations, '[G]oto [I]mplementation')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          map('gD', function()
+            fzf_lua.lsp_typedefs { jump_to_single_result = true, cwd_only = false }
+          end, 'Type [D]efinition')
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('<leader>ds', fzf_lua.lsp_document_symbols, '[D]ocument [S]ymbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('<leader>ws', fzf_lua.lsp_live_workspace_symbols, '[W]orkspace [S]ymbols')
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
@@ -524,10 +526,6 @@ require('lazy').setup({
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-
-          -- WARN: This is not Goto Definition, this is Goto Declaration.
-          --  For example, in C this would take you to the header.
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
@@ -738,7 +736,6 @@ require('lazy').setup({
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
---
+++ lua/custom/plugins/fzf.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    'ibhagwan/fzf-lua',
+    -- optional for icon support
+    dependencies = { 'echasnovski/mini.icons' },
+    opts = {},
+    config = function()
+      local fzf_lua = require 'fzf-lua'
+      fzf_lua.setup()
+      vim.keymap.set('n', '<C-p>', fzf_lua.files, { desc = 'Find files' })
+      vim.keymap.set('i', '<C-p>', fzf_lua.files, { desc = 'Find files' })
+      vim.keymap.set('n', '<C-g>', fzf_lua.live_grep_glob, { desc = 'Grep' })
+      vim.keymap.set('i', '<C-g>', fzf_lua.live_grep_glob, { desc = 'Grep' })
+      vim.keymap.set('n', '<leader>o', fzf_lua.lsp_document_symbols, { desc = 'Document Symbols' })
+      vim.keymap.set('n', '<leader>sk', fzf_lua.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>sf', fzf_lua.oldfiles, { desc = '[S]earch Old [F]iles' })
+      vim.keymap.set('n', '<leader>ss', fzf_lua.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', fzf_lua.grep_cWORD, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', fzf_lua.live_grep_glob, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sd', fzf_lua.diagnostics_workspace, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sr', fzf_lua.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader><leader>', fzf_lua.buffers, { desc = '[ ] Find existing buffers' })
+
+      -- Shortcut for searching your Neovim configuration files
+      vim.keymap.set('n', '<leader>sn', function()
+        fzf_lua.files { cwd = vim.fn.stdpath 'config' }
+      end, { desc = '[S]earch [N]eovim files' })
+    end,
+  },
+}
