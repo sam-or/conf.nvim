0ceda009ed6c967e97e13996b75a12911ff0f1f1
Sam Moore <sam.moore@penten.com>
2025-01-21 17:11:10 +1100

    Add Snacks global

--- init.lua
+++ init.lua
@@ -424,6 +424,7 @@ require('lazy').setup({
       library = {
         -- Load luvit types when the `vim.uv` word is found
         { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        { path = 'snacks.nvim', words = { 'Snacks' } },
       },
     },
   },
